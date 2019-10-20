7894bc052f64d4d871b0c85f252083b3c04e41fb
pierre <ppinet9@gmail.com>
2019-10-20 22:55:21 +0200

    fixed typoo

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ from sliding import *
 
 
 #location_neighborhood_study()
-#test_optimization_a(sliding_optimization_1,3600)
+test_optimization_a(sliding_optimization_1,3600)
 #test_optimization_a(sliding_optimization_2,3600)
-sliding_optimization_1(60,"a1_1")
