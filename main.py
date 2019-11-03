370e07fb7730414af722e2059031ec5c398fcd40
pierre <ppinet9@gmail.com>
2019-11-03 22:31:03 +0100

    added more resolution tets

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ from tests import *
 
 
 #location_neighborhood_study()
-compare_optimization_a("solutions//","sliding_1","sliding_2")
+#compare_optimization_a("solutions//","sliding_1","sliding_2")
+#test_optimization_b(sliding_optimization_1,3600)
+test_optimization_b(sliding_optimization_2,3600)
