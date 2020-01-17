9a2c4c09536dd0bcda32683dc96eef6d32239603
pierre <ppinet9@gmail.com>
2020-01-17 12:42:55 +0100

    added pseudo interface

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from checker import *
 from assesser import *
 from sliding import *
 from tests import *
+from menu import *
 
-
-
-
-#location_neighborhood_study()
-#compare_optimization_a("solutions//","sliding_1","sliding_2")
 #test_movement_total_delta_assesser("a2_2",10)
-test_all_speed()
+main_menu()
